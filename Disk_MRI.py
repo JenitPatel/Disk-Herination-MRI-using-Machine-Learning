5e25776870f92aec15e3f23e5f4fec23e8a53fd7
JenitPatel <JenitPatel@users.noreply.github.com>
2022-06-09 16:38:59 +0100

    Add files via upload

---
+++ Disk_MRI.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import os.path
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+
+from sklearn.metrics import confusion_matrix, classification_report
+
+image_dir = Path('C:\\Users\\Jenit\\Desktop\\MRI scans 0-200')
+
+filepaths = list(image_dir.glob(r'**/*.png'))
+labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
+
+filepaths = pd.Series(filepaths, name='Filepath').astype(str)
+labels = pd.Series(labels, name='Label')
+
+image_df = pd.concat([filepaths, labels], axis=1)
+
+print(image_df['Label'].value_counts())
+print(image_df)
+
+train_df, test_df = train_test_split(image_df, train_size=0.7, shuffle=True, random_state=1)
+
+train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
+    validation_split=0.2
+)
+
+test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input
+)
+
+train_images = train_generator.flow_from_dataframe(
+    dataframe=train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='training'
+)
+
+val_images = train_generator.flow_from_dataframe(
+    dataframe=train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='validation'
+)
+
+test_images = test_generator.flow_from_dataframe(
+    dataframe=test_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=False
+)
+
+pretrained_model = tf.keras.applications.MobileNetV2(
+    input_shape=(224, 224, 3),
+    include_top=False,
+    weights='imagenet',
+    pooling='avg'
+)
+
+pretrained_model.trainable = False
+
+inputs = pretrained_model.input
+x = tf.keras.layers.Dense(128, activation='relu')(pretrained_model.output)
+x = tf.keras.layers.Dense(128, activation='relu')(x)
+outputs = tf.keras.layers.Dense(2, activation='softmax')(x)
+
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_images,
+    validation_data=val_images,
+    epochs=100,
+    callbacks=[
+        tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=3,
+            restore_best_weights=True
+        )
+    ]
+)
+
+results = model.evaluate(test_images, verbose=0)
+predictions = np.argmax(model.predict(test_images), axis=1)
+
+class_names = list(test_images.class_indices.keys())
+
+cm = confusion_matrix(test_images.labels, predictions, labels=np.arange(2))
+clr = classification_report(test_images.labels, predictions, labels=np.arange(2), target_names=class_names)
+
+print("Test Accuracy: {:.2f}%".format(results[1] * 100))
+
+plt.figure(figsize=(8, 8))
+sns.heatmap(cm, annot=True, fmt='g', vmin=0, cmap='Blues', cbar=False)
+plt.xticks(ticks=np.arange(2) + 0.5, labels=class_names, rotation=90)
+plt.yticks(ticks=np.arange(2) + 0.5, labels=class_names, rotation=0)
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.show()
+
+print("Classification Report:\n----------------------\n", clr)
